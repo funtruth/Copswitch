44145c91c603346c925c6fcbb0dbeaec27200652
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-08-23 17:50:27 -0600

    Works on Nathan's Computer

--- android/app/src/main/java/com/huddle/MainApplication.java
+++ android/app/src/main/java/com/huddle/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
