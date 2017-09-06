02b1e80f9db122f371822134c794bfc13254e04c
ouoru <30517327+ouoru@users.noreply.github.com>
2017-09-06 14:25:09 -0400

    Delete Test

--- index.android.js
+++ index.android.js
@@ -8,20 +8,6 @@ import { createRootNavigator } from "./router";
 import { isSignedIn } from "./app/auth";
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          signedIn: false,
-          checkedSignIn: false
-        };
-      }
-    
-    componentWillMount() {
-    isSignedIn()
-        .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
-        .catch(err => alert("An error occurred"));
-    }
 
     render(){
         const { checkedSignIn, signedIn } = this.state;
