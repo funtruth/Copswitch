4818e817f156795133570a87466623884dfb35d6
ouoru <30517327+ouoru@users.noreply.github.com>
2017-10-05 12:34:18 -0400

    Misc

---
+++ app/misc/todolist.js
@@ -0,0 +1,15 @@
+
+
+
+
+//Night Phase Algorithm
+    //Update playernum properly
+
+//Messaging System
+    //Action Log
+    //Notifications
+    //
+
+//Am I Mafia Flag
+
+//Chopping Block player Name
