ef909f84b1c6f670aed1ff142e8f5e1f3505f99c
Hannah Park <hannahpark@Hannahs-MacBook-Air-5.local>
2017-10-28 16:58:07 -0400

    iOS Changes working

--- android/app/src/main/java/com/huddle/MainApplication.java
+++ android/app/src/main/java/com/huddle/MainApplication.java
@@ -3,6 +3,8 @@ package com.huddle;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
@@ -50,6 +52,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAnalyticsPackage(),
           new VectorIconsPackage(),
           
           new RNFirebasePackage(),  // <-- Add this line
