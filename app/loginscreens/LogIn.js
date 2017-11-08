e3184c48e6302f639149af7427ee0e8d703a9fc3
ouoru <30517327+ouoru@users.noreply.github.com>
2017-11-08 13:35:32 -0500

    Login Updated

--- app/loginscreens/LogIn.js
+++ app/loginscreens/LogIn.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { 
-    View, 
-    Keyboard, 
-    AsyncStorage,
-    TextInput,
-    TouchableOpacity,
-    TouchableWithoutFeedback,
+    View
 } from "react-native";
-import { Button } from "react-native-elements";
+
 import { onSignIn } from "../auth";
+import { MenuButton } from '../components/MenuButton.js';
 
 import firebase from '../firebase/FirebaseController.js';
 import colors from '../misc/colors.js';
@@ -37,272 +33,19 @@ export class Splash extends React.Component {
     }
 
     render(){
-        return <TouchableWithoutFeedback 
-            style = {{ flex:1 }}
-            onPress={()=>{ Keyboard.dismiss() }}>
-            <View style = {{flex:1,backgroundColor:colors.background,justifyContent:'center',alignItems:'center'}}>
-                <View style = {{flex:0.9}}/>
-                <View style = {{justifyContent: 'center', alignItems:'center', flexDirection: 'row' }}>
-                    <View style = {{flex:0.75}}>
-                    <Button
-                        title="Continue Anonymously"
-                        fontFamily='ConcertOne-Regular'
-                        color={colors.font}
-                        borderRadius={10}
-                        backgroundColor={colors.main}
-                        onPress={()=>{
-                            this._signInAnon();
-                        }}
-                    /></View>
-
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
-    }
-};
-
-export class SignIn extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.backButtonListener = null;
-
-        this.state = { 
-            email: '', 
-            password: '',
-            };
-      }
-
-    _logIn(){
-        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then(() => {
-            onSignIn();
-            this.props.navigation.navigate("SignedIn");
-            Keyboard.dismiss();
-        }).catch((error)=> { alert('Login Failed') });
-    }
-
-    _signInAnon(){
-        firebase.auth().signInAnonymously().then(() => {
-            onSignIn();
-            this.props.navigation.dispatch(
-                NavigationActions.reset({
-                    key: null,
-                    index: 0,
-                    actions: [
-                      NavigationActions.navigate({ routeName: 'SignedIn'})
-                    ]
-                })
-            )
-        })
-    }
-
-    render(){
-        return <TouchableWithoutFeedback 
-            style = {{ flex:1 }}
-            onPress={()=>{ Keyboard.dismiss() }}>
-            <View style = {{flex:1,backgroundColor:colors.background,justifyContent:'center',alignItems:'center'}}>
-                
-                
-                <View style = {{ justifyContent: 'center', flexDirection: 'row' }}>
-                    <TextInput
-                        placeholder="Email Address ..."
-                        placeholderTextColor = {colors.font}
-                        style={{
-                            backgroundColor: colors.background,
-                            flex:0.6,
-                            fontFamily:'ConcertOne-Regular',
-                            fontSize:22,
-                            color:colors.font,
-                        }}
-                        value={this.state.email}
-                        keyboardType = 'email-address'
-                        onChangeText = {(text) => {this.setState({email: text})}}
-                        onSubmitEditing = {()=>this.refs['password'].focus()}
-                    />
-                </View>
-
-                <View style = {{ justifyContent: 'center', flexDirection: 'row', marginBottom:10 }}>
-                    <TextInput
-                        ref='password'
-                        placeholder="Password ..."
-                        placeholderTextColor = {colors.font}
-                        style={{
-                            backgroundColor: colors.background,
-                            flex:0.6,
-                            fontFamily:'ConcertOne-Regular',
-                            fontSize:22,
-                            color:colors.font,
-                        }}
-                        value={this.state.password}
-                        secureTextEntry = {true}
-                        onChangeText = {(text) => {this.setState({password: text})}}
-                        onSubmitEditing = {()=>{Keyboard.dismiss()}}
-                    />
-                </View>
-
-                <View style = {{ justifyContent: 'center', alignItems:'center', flexDirection: 'row' }}>
-                    <View style = {{flex:0.75}}>
-                    <Button
-                        title="Log In"
-                        fontFamily='ConcertOne-Regular'
-                        color={colors.font}
-                        fontSize={30}
-                        borderRadius={15}
-                        backgroundColor={colors.main}
-                        onPress={()=>{
-                            this._logIn();
-                        }}
-                    /></View>
-                </View>
-
-                <View style ={{flex:0.8}}/>
-
-                <View style = {{justifyContent: 'center', alignItems:'center', flexDirection: 'row', marginBottom:5 }}>
-                    <View style = {{flex:0.75}}>
-                    <Button
-                        title="Sign Up"
-                        fontFamily='ConcertOne-Regular'
-                        color={colors.font}
-                        fontSize={25}
-                        borderRadius={15}
-                        backgroundColor={colors.main}
-                        onPress={() => this.props.navigation.navigate("SignUp")}
-                    /></View>
-                </View>
-                
-                <View style = {{justifyContent: 'center', alignItems:'center', flexDirection: 'row' }}>
-                    <View style = {{flex:0.75}}>
-                    <Button
-                        title="Continue Anonymously"
-                        fontFamily='ConcertOne-Regular'
-                        color={colors.font}
-                        borderRadius={10}
-                        backgroundColor={colors.main}
-                        onPress={()=>{
-                            this._signInAnon();
-                        }}
-                    /></View>
-
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
-    }
-};
-
-export class SignUp extends React.Component {
-    
-    constructor(props){
-        super(props);
-        this.state = { 
-            email: '', 
-            password: '',
-            confirmpassword: '',
-        };
-    }
-    
-    //Creates user and Signs them in
-    _SignUpProcess(email,password){
-      firebase.auth().createUserWithEmailAndPassword(
-        email,password).then(() =>
-        {
-            firebase.auth().signInWithEmailAndPassword(
-            email, password).then(() => 
-                {
-                    onSignIn();
-                    this.props.navigation.navigate("SignedIn");
-                    Keyboard.dismiss();
-                }).catch((error)=> {
-                    alert('Login Failed');
-                });
-    
-        }).catch((error) => {
-            alert('Account Creation Failed.');
-        });
-      }
-    
-    render(){
-        return <TouchableWithoutFeedback 
-        style = {{ flex:1 }}
-        onPress={()=>{ Keyboard.dismiss() }}>
-        <View style = {{flex:1,backgroundColor:colors.background,justifyContent:'center',alignItems:'center'}}>
-            
-            
-            <View style = {{ justifyContent: 'center', flexDirection: 'row' }}>
-                <TextInput
-                    placeholder="Email Address ..."
-                    placeholderTextColor = {colors.font}
-                    style={{
-                        backgroundColor: colors.background,
-                        flex:0.6,
-                        fontFamily:'ConcertOne-Regular',
-                        fontSize:18,
-                        color:colors.font,
-                    }}
-                    value={this.state.email}
-                    keyboardType = 'email-address'
-                    onChangeText = {(text) => {this.setState({email: text})}}
-                    onSubmitEditing = {()=>this.refs['password'].focus()}
-                />
-            </View>
-
-            <View style = {{ justifyContent: 'center', flexDirection: 'row' }}>
-                <TextInput
-                    ref='password'
-                    placeholder="Password ..."
-                    placeholderTextColor = {colors.font}
-                    style={{
-                        backgroundColor: colors.background,
-                        flex:0.6,
-                        fontFamily:'ConcertOne-Regular',
-                        fontSize:18,
-                        color:colors.font,
-                    }}
-                    value={this.state.password}
-                    secureTextEntry = {true}
-                    onChangeText = {(text) => {this.setState({password: text})}}
-                    onSubmitEditing = {()=>this.refs['confirm'].focus()}
-                />
-            </View>
-
-            <View style = {{ justifyContent: 'center', flexDirection: 'row', marginBottom:10 }}>
-                <TextInput
-                    ref='confirm'
-                    placeholder="Confirm ..."
-                    placeholderTextColor = {colors.font}
-                    style={{
-                        backgroundColor: colors.background,
-                        flex:0.6,
-                        fontFamily:'ConcertOne-Regular',
-                        fontSize:18,
-                        color:colors.font,
-                    }}
-                    value={this.state.confirm}
-                    secureTextEntry = {true}
-                    onChangeText = {(text) => {this.setState({confirm: text})}}
-                    onSubmitEditing = {()=>{Keyboard.dismiss()}}
-                />
-            </View>
-
-            <View style = {{ justifyContent: 'center', alignItems:'center', flexDirection: 'row' }}>
-                <View style = {{flex:0.75}}>
-                <Button
-                    title="Create Account"
-                    fontFamily='ConcertOne-Regular'
-                    color={colors.font}
-                    fontSize={30}
-                    borderRadius={15}
-                    backgroundColor={colors.main}
-                    onPress={()=>{
-                        this._SignUpProcess(this.state.email,this.state.password);
-                    }}
-                /></View>
-            </View>
-
-            <View style = {{flex:0.5}}/>
-
+        return <View style = {{flex:1,backgroundColor:colors.background,
+            justifyContent:'center',alignItems:'center'}}>
+            <View style = {{flex:0.82}}/>
+            <MenuButton
+                viewFlex = {0.12}
+                flex = {0.9}
+                title="Continue Anonymously"
+                fontSize={25}
+                onPress={()=>{
+                    this._signInAnon();
+                }}
+            />
+            <View style = {{flex:0.06}}/>
         </View>
-    </TouchableWithoutFeedback>
-            
     }
 };
