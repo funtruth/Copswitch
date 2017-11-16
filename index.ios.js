580906239d7f9810910c435654622e755e7b96bd
ouoru <30517327+ouoru@users.noreply.github.com>
2017-11-16 16:33:17 -0500

    index.ios.js copied

--- index.ios.js
+++ index.ios.js
@@ -3,37 +3,45 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 
 import { createRootNavigator } from "./router";
-import { isSignedIn } from "./app/auth";
+import { isSignedIn, isInGame } from "./app/auth";
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        
+    
         this.state = {
-        signedIn: false,
-        checkedSignIn: false,
+          signedIn: false,
+          checkedSignIn: false,
+
+          inGame: false,
+          checkedInGame: false,
         };
-    }
+      }
     
     componentWillMount() {
-        
+
         isSignedIn()
-        .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
-        .catch(err => alert("An error occurred"));
+            .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+            .catch(err => alert("An error occurred"));
+
+        isInGame()
+            .then(res => this.setState({ inGame: res, checkedInGame: true }))
+            .catch(err => alert("An error occurred"));
+        
     }
-    
+
     render(){
-        const { checkedSignIn, signedIn } = this.state;
+        const { checkedSignIn, signedIn,
+                checkedInGame, inGame } = this.state;
         
         // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
-        if (!checkedSignIn) {
+        if (!checkedSignIn || !checkedInGame) {
             return null;
         }
-        
-        const Layout = createRootNavigator(signedIn);
+    
+        const Layout = createRootNavigator(signedIn, inGame);
         return <Layout />;
     }
 }
 
 AppRegistry.registerComponent('Huddle', () => App);
-
