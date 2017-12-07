6930d62653949a75346431e7e89048b4d2b2d885
ouoru <30517327+ouoru@users.noreply.github.com>
2017-12-07 11:44:02 -0500

    version^8

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-
 import { AppRegistry } from 'react-native';
-
 import { createRootNavigator } from "./router";
-import { isSignedIn, isInGame } from "./app/auth";
 
 export default class App extends React.Component {
     constructor(props) {
