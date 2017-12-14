9e4943e28816c87c46be6d36ff6887dbdb52567c
ouoru <30517327+ouoru@users.noreply.github.com>
2017-12-14 03:14:09 -0500

    Reset Attept

--- app/components/Header.js
+++ app/components/Header.js
@@ -19,12 +19,12 @@ render() {
 
     return ( 
         <View style = {{height:this.height*0.1, flexDirection:'row', marginTop:10}}>
-                <View style = {{flex:0.25, justifyContent:'center'}}>
-                    <TouchableOpacity onPress = {this.props.onPress}>
-                        <MaterialCommunityIcons name='chevron-left' 
-                        style={{ color:colors.dshadow, fontSize: 30, alignSelf:'center'}}/>
-                    </TouchableOpacity>
-                </View>
+            <View style = {{flex:0.25, justifyContent:'center'}}>
+                <TouchableOpacity onPress = {this.props.onPress}>
+                    <MaterialCommunityIcons name='chevron-left' 
+                    style={{ color:colors.dshadow, fontSize: 30, alignSelf:'center'}}/>
+                </TouchableOpacity>
+            </View>
             <View style = {{flex:0.5, justifyContent:'center', alignItems:'center'}}>
                 <Text style = {{
                     fontFamily:'ConcertOne-Regular',
